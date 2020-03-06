0e9973c053a9445965d0fd08714d69eb416b060c
sheetalc818 <sheetalc818@gmail.com>
2020-03-06 11:22:48 +0530

    Added code to sort the census data by state

--- src/main/java/censusanalyser/CensusAnalyserException.java
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -5,7 +5,7 @@ public class CensusAnalyserException extends Exception {
 
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE;
+        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE, NO_CENSUS_DATA;
     }
 
     public ExceptionType type;
